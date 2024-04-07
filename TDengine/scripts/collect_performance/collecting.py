d2680dd6c1fcb52613c83261f6438a848ef95ede
lcax200000 <xiaoxiaoling20@126.com>
2024-04-07 22:11:15 +0800

    STABLE Table Transformation

--- TDengine/scripts/collect_performance/collecting.py
+++ TDengine/scripts/collect_performance/collecting.py
@@ -7,7 +7,7 @@ import sys
 from taosrest import connect, TaosRestConnection, TaosRestCursor
 cancel_tmr = False
 conn = {}
-def start(uid:int):
+def start():
     # 获取CPU信息
     cpus = psutil.cpu_times(percpu=True)
     # 获取总体CPU使用率，如果想要每个CPU的使用率，可以使用psutil.cpu_percent(interval=None, percpu=True)
@@ -20,7 +20,7 @@ def start(uid:int):
     disks = psutil.disk_io_counters(perdisk=True)
     # 获取网络I/O信息
     net_io = psutil.net_io_counters()
-    sqlCmd = "insert into server_performance  values (now,%d,%f,%d,%d,%d)" % (uid, cpu_usage, memory.available/1000, net_io.bytes_recv/1000, net_io.bytes_sent/1000)
+    sqlCmd = "insert into server_performance" + sys.argv[1] + " using server_performance TAGS (%s) values (now,%f,%d,%d,%d)" % (sys.argv[1], cpu_usage, memory.available/1000, net_io.bytes_recv/1000, net_io.bytes_sent/1000)
     print(conn.execute(sqlCmd))
 
 
@@ -29,7 +29,7 @@ def heart_beat():
     # 打印当前时间
     print(time.strftime('%Y-%m-%d %H:%M:%S'))
     if not cancel_tmr:
-        start(int(sys.argv[1]))
+        start()
         # 每隔3秒执行一次
         threading.Timer(3, heart_beat).start()
 
@@ -39,7 +39,7 @@ if __name__ == '__main__':
     #                     port=6030,
     #                     database="log")
     #conn = taosws.connect("taosws://root:taosdata@192.168.1.200:6041")
-    conn = connect(url="http://192.168.1.200:6041",
+    conn = connect(url="http://192.168.56.31:6041",
                user="root",
                password="taosdata",
                timeout=30,
--- TDengine/scripts/collect_performance/create_table.py
+++ TDengine/scripts/collect_performance/create_table.py
@@ -1,19 +1,19 @@
 from taosrest import connect, TaosRestConnection, TaosRestCursor
-
+import sys
 
 def create():
     # all parameters are optional.
     # if database is specified,
     # then it must exist.
-    conn = connect(url="http://192.168.1.200:6041",
+    conn = connect(url="http://192.168.56.31:6041",
                user="root",
                password="taosdata",
                timeout=30,
                database="log")
     print('conn info:', conn)
 
-    conn.execute('CREATE TABLE server_performance (ts TIMESTAMP, uid int,cpu_usage float, memory_available int, '
-                 'bytes_recv int, bytes_sent int)')
+    conn.execute('CREATE STABLE IF NOT EXISTS server_performance (ts TIMESTAMP, cpu_usage float, memory_available int, '
+                 'bytes_recv int, bytes_sent int) TAGS (uid int)')
     conn.close()
 
 
