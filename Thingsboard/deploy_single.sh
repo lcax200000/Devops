47f5a5ac8c8b96ef73e7863f5e927b659ca16ee1
lcax200000 <xiaoxiaoling20@126.com>
2024-04-08 17:38:35 +0800

    deploy  single node  thingsboard

---
+++ Thingsboard/deploy_single.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# 创建 Docker volume
+docker volume create tb-data
+docker volume create tb-logs
+
+# 拉取 Docker Compose 配置
+docker-compose pull
+
+# 启动 Docker 容器
+docker-compose up -d
+
+# 查看特定容器日志
+docker-compose logs -f mytb
